1fe169ee17efd8074aa44a4ced2bab71a94942a2
kim_hyun_soo <tomy9729@naver.com>
2021-07-21 01:31:39 +0900

    Instagram ver.20210721

--- django-with-react-rev2/moz1e/settings/common.py
+++ django-with-react-rev2/moz1e/settings/common.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from os.path import abspath, dirname
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,15 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third Apps
+    'debug_toolbar',
+    #Locals Apps
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+INTERNAL_IPS = ['127.0.0.1',]
--- django-with-react-rev2/moz1e/wsgi.py
+++ django-with-react-rev2/moz1e/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moz1e.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moz1e.settings.prod')
 
 application = get_wsgi_application()
