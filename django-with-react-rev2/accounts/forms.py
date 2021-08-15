d2816827930acd7b37aa317ec37ea7a5e348801d
kim_hyun_soo <tomy9729@naver.com>
2021-08-15 17:20:14 +0900

    Revert "Revert "7-10""
    This reverts commit 6c9a083802cf5f8cf3c91fd5c4b7e01cd272e061.

--- django-with-react-rev2/accounts/forms.py
+++ django-with-react-rev2/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 import django
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, PasswordChangeForm as AuthPasswordChangeForm
+)
 from .models import User
 
 class SingupForm(UserCreationForm):
@@ -26,3 +28,13 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['avatar','first_name','last_name','website_url','bio','phone_number','gender']
+
+class PasswordChangeForm(AuthPasswordChangeForm) : 
+    def clean_new_password2(self) :
+        old_password = self.cleaned_data.get('old_password')
+        new_password1 = self.cleaned_data.get('new_password1')
+        #new_password2 = super().clean_new_password2()
+        if old_password and new_password1 : 
+            if old_password == new_password1 : 
+                raise forms.ValidationError("동일한 암호로 변경할 수 없습니다.")
+        return new_password1
--- django-with-react-rev2/accounts/urls.py
+++ django-with-react-rev2/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/',views.login, name='login'),
     path('logout/',views.logout, name='logout'),
+    path('password_change/', views.password_change, name='password_change'),
     path('signup/',views.signup, name='signup'),   
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
