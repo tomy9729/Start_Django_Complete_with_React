31f697e54ceabff1c67087d1896315c678d64305
kim_hyun_soo <tomy9729@naver.com>
2021-08-18 21:08:24 +0900

    7-13

--- django-with-react-rev2/instagram/views.py
+++ django-with-react-rev2/instagram/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib import messages
 from instagram.forms import PostForm
 from django.contrib.auth.decorators import login_required
@@ -27,4 +28,14 @@ def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, "instagram/post_detail.html",{
         "post" : post,
+    })
+
+
+def user_page(request, username) : 
+    page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
+    post_list = Post.objects.filter(author=page_user)
+    return render(request, "instagram/user_page.html",{
+        "page_user" : page_user,
+        "post_list" : post_list,
+
     })
