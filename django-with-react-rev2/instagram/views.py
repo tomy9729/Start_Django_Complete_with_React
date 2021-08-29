729e0a13d0addb674a8db61e93d9a8ced2c4c2d0
kim_hyun_soo <tomy9729@naver.com>
2021-08-29 20:43:40 +0900

    7-18 19

--- django-with-react-rev2/instagram/views.py
+++ django-with-react-rev2/instagram/views.py
@@ -1,15 +1,21 @@
+from datetime import timedelta
 from django.contrib.auth import get_user_model
 from django.contrib import messages
 from instagram.forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 from .models import Tag, Post
 from .forms import PostForm
 
 @login_required
 def index(request) : 
-    post_list = Post.objects.all().filter(Q(author=request.user) | Q(author__in=request.user.following_set.all()))
+    timesince = timezone.now()-timedelta(days=3)
+    post_list = Post.objects.all().filter(Q(author=request.user) | Q(author__in=request.user.following_set.all()))\
+        .filter(
+            created_at__gte = timesince
+        )
 
     suggested_user_list=get_user_model().objects.all().exclude(pk=request.user.pk).exclude(pk__in=request.user.following_set.all())[:5]
 
@@ -47,8 +53,15 @@ def user_page(request, username) :
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count  = post_list.count()
+
+    if request.user.is_authenticated : 
+        is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+    else : 
+        is_follow = False
+    
     return render(request, "instagram/user_page.html",{
         "page_user" : page_user,
         "post_list" : post_list,
         "post_list_count" : post_list_count,
+        "is_follow" : is_follow,
     })
